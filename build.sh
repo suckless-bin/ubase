9882a92b802ea683159cca980f6bea8c996e76ab
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-31 14:54:28 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ git clone --depth=1 https://git.suckless.org/$repo/
 cp config.mk  $repo/
 cp Makefile  $repo/
 cd $repo
-make DESTDIR="$PWD/pkg" install
+make DESTDIR="$PWD/pkg" ubase-box-install
 tar -czvf $repo-static.tar.gz -C "$PWD/pkg" .
 
 
